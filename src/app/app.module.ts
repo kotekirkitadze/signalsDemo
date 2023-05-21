ec18d688313c3aa8b156e5dd621796630625b003
kotekirkitadze <kotekirkitadze@gmail.com>
2023-05-21 15:26:09 +0400

    generate components, services and model.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ShellComponent } from './components/shell/shell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailsComponent,
+    ShellComponent
   ],
   imports: [
     BrowserModule,
